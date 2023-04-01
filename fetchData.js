569917428ad36ab3f781d4fd374ea6ddce1dd2a2
awal99 <awalsulemana3@gmail.com>
2023-04-01 12:23:22 +0000

    updated fetch data

--- fetchData.js
+++ fetchData.js
@@ -2,7 +2,19 @@
 
 const data = require('./data.js')
 
+//lets load all the data into the device localstorage so that we can update them when necessary
+//e.g when user makes contribution.
+const key = "CONTRACT_STORE";
+localStorage.setItem(key,JSON.stringify(data))
+
+function fetchAllDataFromStore(){
+    let all_data = localStorage.getItem(key);
+    return JSON.parse(all_data);
+}
+
 function fetchTopContributors(){
-    return data.topHundredContributors
+    //get data from store
+    let all_data = fetchAllDataFromStore()
+    return all_data.topHundredContributors;
 }
 
