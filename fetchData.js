9d73449dcbf92ad8e5604c39405d7041df317e4b
wevs <68214494+dopewevmond@users.noreply.github.com>
2023-04-01 12:16:18 +0000

    utility functions to get the data

--- fetchData.js
+++ fetchData.js
@@ -6,3 +6,26 @@ function fetchTopContributors(){
     return data.topHundredContributors
 }
 
+function getLevelsDetails(){
+    return data.levelsDetails
+}
+
+function getUsersTotalContributions(){
+    return data.yourTotalContributions
+}
+
+function getCurrentLevel(){
+    return data.currentLevel
+}
+
+function getAllPublicSaleDetails(){
+    return data.publicSaleDetails
+}
+
+module.exports = {
+    fetchTopContributors,
+    getLevelsDetails,
+    getUsersTotalContributions,
+    getCurrentLevel,
+    getAllPublicSaleDetails
+}
