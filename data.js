8e8a6e5850f7a617fa1d2a08182edcdfabf3a2e7
Asad <clarkpeace.adams@gmail.com>
2023-04-03 15:27:22 +0000

    changed data format of levelsData, changed values from string to numbers

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const data = {
-  leaderboardData:[
+  leaderboardData: [
     { rank: 1, address: "0x1234567890abcdef1234567890abcdef12345678", amount: 5000 },
     { rank: 2, address: "0xabcdef1234567890abcdef1234567890abcdef12", amount: 4000 },
     // { rank: 3, address: "0x1234567890abcdef1234567890abcdef1234567dd", amount: 3000 },
@@ -16,57 +16,61 @@ const data = {
     // { rank: 100, address: "0xabcdef1234567890abcdef1234567890abcdef12", amount: 97 },
     // { rank: 125, address: "0x1234567890abcdef1234567890abcdef12345678", amount: 96 },
     // { rank: 126, address: "0xabcdef1234567890abcdef1234567890abcdef12", amount: 95 },
-],
-  levelsData:[
+  ],
+  levelsData: [
     {
-        "level": 1,
-        "max_raise": "1,000,000",
-        "ratio": "25",
-        "peg_per_level": "10,000",
-        "peg_price":0.5,
-        "pls_contribute": 250,
-        "price_per_pls": "0.025",
-        "usdc_contribute": 750,
-        "price_per_eth": "0.0348",
-        "filled": 10
+      "level": 1,
+      "max_raise": 1000000,
+      "ratio": "25",
+      "peg_per_level": 10000,
+      "peg_price": 0.5,
+      "pls_contribute": 250,
+      "price_per_pls": 0.025,
+      "total_usdc_contribute": 0,
+      "usdc_contribute": 750,
+      "price_per_eth": 0.0348,
+      "filled": 10
     },
     {
-        "level": 2,
-        "max_raise": "2,000,000",
-        "ratio": "25",
-        "peg_per_level": "20,000",
-        "pls_contribute": 0,
-        "peg_price":0.6,
-        "price_per_pls": "0.025",
-        "usdc_contribute": 0,
-        "price_per_eth": "0.0348",
-        "filled": 0
+      "level": 2,
+      "max_raise": 2000000,
+      "ratio": 25,
+      "peg_per_level": 20000,
+      "pls_contribute": 0,
+      "peg_price": 0.6,
+      "price_per_pls": 0.025,
+      "total_usdc_contribute": 0,
+      "usdc_contribute": 0,
+      "price_per_eth": 0.0348,
+      "filled": 0
     },
     {
-        "level": 3,
-        "max_raise": "3,000,000",
-        "ratio": "25",
-        "peg_per_level": "30,000",
-        "pls_contribute": 0,
-        "peg_price":0.7,
-        "price_per_pls": "0.025",
-        "usdc_contribute": 0,
-        "price_per_eth": "0.0348",
-        "filled": 0
+      "level": 3,
+      "max_raise": 3000000,
+      "ratio": 25,
+      "peg_per_level": 30000,
+      "pls_contribute": 0,
+      "peg_price": 0.7,
+      "price_per_pls": 0.025,
+      "total_usdc_contribute": 0,
+      "usdc_contribute": 0,
+      "price_per_eth": 0.0348,
+      "filled": 0
     },
     {
-        "level": 4,
-        "max_raise": "4,000,000",
-        "ratio": "25",
-        "peg_per_level": "40,000",
-        "pls_contribute": 0,
-        "peg_price":0.8,
-        "price_per_pls": "0.025",
-        "usdc_contribute": 0,
-        "price_per_eth": "0.0348",
-        "filled": 0
+      "level": 4,
+      "max_raise": 4000000,
+      "ratio": 25,
+      "peg_per_level": 40000,
+      "pls_contribute": 0,
+      "peg_price": 0.8,
+      "price_per_pls": 0.025,
+      "total_usdc_contribute": 0,
+      "usdc_contribute": 0,
+      "price_per_eth": 0.0348,
+      "filled": 0
     }
-],
+  ],
   yourTotalContributions: {
     pls: 210000,
     usdc: 0,
@@ -88,17 +92,17 @@ const data = {
     fdv: 2210000
   },
 
-  user:{
-    address:"0x1234567890abcdef1234567890abcdef1234567dd",
+  user: {
+    address: "0x1234567890abcdef1234567890abcdef1234567dd",
     contributed_pls: 0,
     contributed_usdc: 0,
     contributed_amount: 0,
     total: 0,
-    contributed_peg:0.65,
-    pegPrice:0
+    contributed_peg: 0.65,
+    pegPrice: 0
   },
-  
-  
+
+
 
 };
 
